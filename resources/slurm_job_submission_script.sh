e993a8ced614f05fb3af89cccddacaa98f7ea04e
jraylmer <j.aylmer@reading.ac.uk>
2026-01-05 14:02:09 +0000

    Update template job submission script

--- resources/slurm_job_submission_script.sh
+++ resources/slurm_job_submission_script.sh
@@ -1,18 +1,19 @@
 #!/bin/bash
 
-# Example SLURM job submission script for NEMO-SI3 compiled with OpenMPI on the
-# University of Reading Academic Computing Cluster 2 (UoR RACC2). This should
-# be placed in a compiled configuration's run directory.
-
 #SBATCH --ntasks=8
 #SBATCH --cpus-per-task=1
 #SBATCH --threads-per-core=1
 #SBATCH --nodes=1-1
+#SBATCH --mem=32G
+#SBATCH --job-name=NEMO
 #SBATCH --output=%x-%j.out
-#SBATCH --time=1:0:0
+#SBATCH --time=01:00:00
 
+# Prepare environment:
+module purge
 module load mpi/openmpi-x86_64
 
-# Do not use srun: the version of OpenMPI on RACC2 does not support it and
-# mpiexec is recommended by OpenMPI anyway:
-mpiexec ./nemo
+export OMPI_MCA_fs_ufs_lock_algorithm=1
+export HDF5_USE_FILE_LOCKING=FALSE
+
+mpirun ./nemo
